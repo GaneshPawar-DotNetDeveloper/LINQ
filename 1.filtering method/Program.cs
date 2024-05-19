cf7dcbcb5d54c8a74059101e86af3dbef675e458
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-05-19 16:51:18 +0530

    Add files via upload

---
+++ 1.filtering method/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.filtering_method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           /* List<student> sc = new List<student>()
+            {
+                new student() {id=1,name="suraj",age=27},
+                new student() {id=2,name="ganesh",age=25},
+                new student() {id=3,name="raj",age=23},
+                new student() {id=4,name="samir",age=29}
+            };
+            Console.WriteLine("------all students-------");
+
+            foreach (student item in sc)
+            {
+                Console.WriteLine($"{item.id} {item.name} {item.age}");
+            }
+*/
+
+
+            // anonymous method:
+          /*  IEnumerable<student> filteredstudent = sc.Where( delegate (student s)
+                {
+                    if(s.age == 23)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                       
+                    }
+                });
+            foreach (student item in filteredstudent)
+            {
+                Console.WriteLine($"id {item.id}  name: {item.name} age : {item.age}");
+            }*/
+
+
+
+
+
+
+
+
+            // by lambda expression:
+            /* IEnumerable<student> name = sc.Where(s => s.age == 29);
+             foreach (student item in name)
+             {
+                 Console.WriteLine($"id:{item.id} name: {item.name} age: {item.age}");
+             }*/
+
+
+           /* // using query method:
+
+           // IEnumerable<student> ww=  this replace with var keyword:
+           var ww= from s in sc
+                   where s.name.StartsWith("g")
+                   select s;
+            foreach (student item in  ww)
+            {
+                Console.WriteLine(item.name);
+            }*/
+
+
+
+            // we have object type array and in that array there is mixed datatype values are there
+            // so i want to fetch only int value with use of where method. 
+
+            List<object> list= new List<object>()
+            {
+                10,"ganesh",10.5f,"BAMD",20,456,"pql"
+            };
+
+
+            //anonymous method
+            /*   var result = list.Where(delegate (object o)
+               {
+                   return o.GetType().Name == "Int32";
+               });
+               foreach (var item in result)
+               {
+                   Console.WriteLine(item);
+               }*/
+            // lamda expression:
+
+            // var results = list.Where(a => a.GetType().Name == "Int32");
+            /*  var results = list.Where(a => a.GetType().Name == "String");
+              foreach (var item in results)
+              {
+                  Console.WriteLine(item);
+
+              }*/
+            // so here to reduce that confusion we can use oftype method:
+
+            var result = list.OfType<float>();
+            foreach (var item in result )
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+    public class student
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+    }
+}
---
+++ 2.SortingMethods/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.SortingMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<student> students = new List<student>()
+            {
+                new student() {rollnumber=1,name="mahesh",age=27},
+                new student() {rollnumber=2,name="ganesh",age=25},
+                new student() {rollnumber=3,name="rajesh",age=23},
+                new student() {rollnumber=4,name="suresh",age=21},
+                new student() {rollnumber=5,name="mangesh",age=23},
+            };
+            Console.WriteLine("\n----All students-----");
+            foreach (student item in students)
+            {
+                Console.WriteLine($"rollnumber :{item.rollnumber}  name: {item.name}  age: {item.age}");
+            }
+
+            Console.WriteLine("\n----sorted on age by acending----");
+            IOrderedEnumerable<student> sorting = students.OrderBy(delegate (student s){ return s.age;});
+            foreach (student item in sorting)
+            {
+                Console.WriteLine($"rollnumber :{item.rollnumber}  name: {item.name}  age: {item.age}");
+
+            }
+
+            Console.WriteLine("\n-----sorted on age by desending");
+            IOrderedEnumerable<student> sortingdes = students.OrderByDescending(s => s.age);
+            foreach (student item in sortingdes)
+            {
+                Console.WriteLine($"rollnumber :{item.rollnumber}  name: {item.name}  age: {item.age}");
+
+            }
+
+            // here age is same for age=23 so i want also sort with name :
+
+
+            Console.WriteLine("\n------sorted  on age descending and name in ascending-----");
+            IOrderedEnumerable<student> sort = students.OrderByDescending(s => s.age).ThenBy(s => s.name);
+            foreach (student item in sort)
+            {
+                Console.WriteLine($"rollnumber :{item.rollnumber}  name: {item.name}  age: {item.age}");
+
+            }
+            Console.WriteLine("\n---sorted on age descending and name in also descending---");
+            IOrderedEnumerable<student>sorty=students.OrderByDescending(s => s.age).ThenByDescending(s => s.name);
+            foreach (student item in sorty)
+            {
+                Console.WriteLine($"rollnumber :{item.rollnumber}  name: {item.name}  age: {item.age}");
+
+            }
+
+            Console.WriteLine("\n---sorted on age Ascending and name in decending");
+            IOrderedEnumerable<student> sor = students.OrderBy(a => a.age).OrderByDescending(b => b.name);
+            foreach (student item in sor)
+            {
+                Console.WriteLine($"rollnumber :{item.rollnumber}  name: {item.name}  age: {item.age}");
+
+            }
+
+            Console.WriteLine("\n---reverse method---");
+
+            IEnumerable<student> reverseing=sorting.Reverse();
+            foreach (student item in reverseing)
+            {
+                Console.WriteLine($"rollnumber :{item.rollnumber}  name: {item.name}  age: {item.age}");
+
+            }
+            Console.WriteLine(" \n we can revrese our collection as it is :");
+
+            students.Reverse();
+            foreach (student item in students)
+            {
+                Console.WriteLine($"rollnumber :{item.rollnumber}  name: {item.name}  age: {item.age}");
+
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+    public class student
+    {
+        public int rollnumber;
+       public string name;
+       public int age;
+
+    }
+}
