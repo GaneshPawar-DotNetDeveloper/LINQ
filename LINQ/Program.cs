196b290914cd2addb636f0535351e06ac6eb0921
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-05-19 16:45:24 +0530

    Add files via upload

---
+++ LINQ/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<student> sc=new List<student>()
+            {
+                new student() {id=1,name="suraj",age=27},
+                new student() {id=2,name="ganesh",age=25},
+                new student() {id=3,name="raj",age=23},
+                new student() {id=4,name="samir",age=29}
+            };
+            Console.WriteLine("------all students-------");
+
+            foreach (student item in sc)
+            {
+                Console.WriteLine($"{item.id} {item.name} {item.age}");
+            }
+
+            // traditional way of syntax:
+            /* List<student> filteredstudent = new List<student>();
+             foreach (student item in sc)
+             {
+                 if(item.age>=25 && item.age <= 30)
+                 {
+                     filteredstudent.Add(item);
+                 }
+             }*/
+            /* Console.WriteLine("----filtered student-----");
+             foreach (student item in filteredstudent)
+             {
+                 Console.WriteLine($"{item.id} {item.name} {item.age}");
+             }
+ */
+            // linq query syntax:
+            /*
+                        IEnumerable<student> filteredstudent = from item in sc where item.age >= 25 &&
+                                                               item.age <= 30 select item;
+
+                        Console.WriteLine("----filtered student-----");
+                        foreach (student item in filteredstudent)
+                        {
+                            Console.WriteLine($"{item.id} {item.name} {item.age}");
+                        }*/
+
+
+            // linq method syntax:
+
+            /*   IEnumerable<student> filteredstudent = sc.Where(delegate (student s)
+               {
+                   return s.age>=28 && s.age<=30;
+               });*/
+
+            // OR
+            Func<student, bool> del = delegate (student s)
+            {
+                return s.age >= 28 && s.age <= 30;
+            };
+            IEnumerable<student> filteredstudent = sc.Where(del);
+
+            Console.WriteLine("----filtered student-----");
+            foreach (student item in filteredstudent)
+            {
+                Console.WriteLine($"{item.id} {item.name} {item.age}");
+            }
+            Console.ReadLine();
+        }  
+    }
+    public class student
+    {
+        public int id {get;set;} 
+        public string name {get;set;} 
+        public int age {get;set;} 
+    }
+}
---
+++ Task/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> name=new List<string>()
+            {
+                "suresh","ganesh","mahesh","shailesh","ramesh"
+            };
+
+            // so  here i want to write code for get name which is start with 's'
+
+            Console.WriteLine("----starts with s----");
+
+            IEnumerable<string> getname = name.Where(delegate (string s)
+            {
+                return s.StartsWith("s");
+            });
+            foreach (string item in getname)
+            {
+                Console.WriteLine(item);
+            }
+
+            // with lambda expression:
+            Console.WriteLine("---starts with g----");
+            IEnumerable<string> names = name.Where(s => s.StartsWith("g"));
+            foreach (string s in names)
+            {
+                Console.WriteLine(s);
+            }
+           
+        List<int> numbers= new List<int>() {10,33,63,56,32,64,76 };
+
+            Console.WriteLine("divisible by 2 ");
+            IEnumerable<int> numbers2 = numbers.Where(delegate (int a)
+            {
+                return  a%2==0;
+            });
+            foreach (int item in numbers2)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("---divisible by 3 ---");
+            IEnumerable<int> div = numbers.Where(b => b%3==0);
+            foreach (int item in div)
+            {
+                Console.WriteLine(item);
+            }
+
+        Console.ReadLine();
+        }
+    }
+}
