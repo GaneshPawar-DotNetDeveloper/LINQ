4efa6caf3883ef42a4f1e6725a2a7edc5474d871
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-05-21 20:53:44 +0530

    Add files via upload

---
+++ 8.1 -ComplexTypeCollection/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8._1__ComplexTypeCollection
+{
+
+    // I want get All Unique data:
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<product> item = new List<product>()
+            {
+                new product(){name="lemon",code=001},
+                new product(){name="Apple",code=002},
+                new product(){name="Apple",code=002},
+                new product(){name="Banana",code=003},
+                new product(){name="Banana",code=003},
+                new product(){name="graps",code=004},
+                new product(){name="clustered",code=005},
+                new product(){name="mango",code=006},
+                new product(){name="Cocunut",code=007},
+                new product(){name="imali",code=008}
+            };
+            Console.WriteLine("----All Products-----");
+            foreach (product i in item)
+            {
+                Console.WriteLine($"name : {i.name}  code : {i.code}");
+            }
+
+
+            Console.WriteLine("----get all unique data--------");
+
+            IEnumerable<product> dd = item.Distinct(new copyproduct());
+
+            // here we can need to create a new class get impliment equals method.
+            foreach (product i in dd)
+            {
+                Console.WriteLine($"name : {i.name}  code : {i.code}");
+            }
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+       
+    }
+    public class product
+    {
+        public string name;
+        public int code;
+    }
+    public class copyproduct : IEqualityComparer<product>
+    {
+        public bool Equals(product x, product y)
+        {
+            if (x == null || y == null)
+                return false;
+
+            return x.name == y.name && x.code == y.code;
+        }
+
+
+
+
+
+        public int GetHashCode(product obj)
+        {
+            if (obj == null)
+                return 0;
+
+            // Combine hash codes of the properties
+            int hashName = obj.name == null ? 0 : obj.name.GetHashCode();
+            int hashCode = obj.code.GetHashCode();
+            return hashName ^ hashCode;
+        }
+    }
+}
---
+++ 8.SetOperator/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8.SetOperator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> num = new List<int>() { 1, 2, 3, 2, 1, 1, 4, 5, 5 };
+            Console.WriteLine("------All elemets------");
+            foreach (int i in num)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+
+            // so here i want unique value 
+            // so for that we use distinct
+            Console.WriteLine("-----Distinct-----");
+            IEnumerable<int> n = num.Distinct();
+            foreach (var item in n)
+            {
+                Console.Write(item);
+            }
+            Console.WriteLine();
+            Console.WriteLine("------Except-------");
+            List<int> list1 = new List<int>() { 1, 2, 3, 4, 5 };
+            List<int> list2 = new List<int>() { 4, 5, 6, 7 };
+
+            IEnumerable<int> numb = list1.Except(list2);
+            foreach (var item in numb)
+            {
+                Console.Write(item);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("-----Intersect------");
+         IEnumerable<int> s=   list1.Intersect(list2);
+            foreach (var item in s)
+            {
+                Console.Write(item);
+            }
+            Console.WriteLine();
+            Console.WriteLine("-----union------");
+
+      IEnumerable<int> j=      list1.Union(list2);
+            foreach (var item in j)
+            {
+                Console.Write(item);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("-----Concat-----");
+        IEnumerable<int>  n2=  list1.Concat(list2);
+            foreach (int item in n2)
+            {
+                Console.Write(item);
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
