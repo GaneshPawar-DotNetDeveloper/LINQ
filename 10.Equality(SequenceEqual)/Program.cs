d9695d2802168b74a6f9ec45df3385d2dda9956b
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-05-21 21:01:40 +0530

    Add files via upload

---
+++ 10.Equality(SequenceEqual)/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Equality_SequenceEqual_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // sequence same he ki nahi ye check karane ke liye hum use karenge.
+            // return boolean value
+            // there are two overloads
+            // isme element+sequence same hona mendatary he.
+
+            List<int> num1 = new List<int>() {1,2,3,4,5 };
+            List<int> num2 = new List<int>() {1,2,3,4,5 };
+        bool i= num1.SequenceEqual(num2);
+            Console.WriteLine(i);
+            Console.WriteLine("------");
+
+
+            List<int> num11 = new List<int>() { 1, 2, 3, 4, 5 };
+            List<int> num22 = new List<int>() { 1, 3, 2, 4, 5 };
+
+            bool n= num11.SequenceEqual(num22);
+            Console.WriteLine(n);
+            Console.WriteLine("-------------");
+
+            List<int> num111 = new List<int>() { 1, 2, 3, 4, 5 };
+            List<int> num222 = new List<int>() { 1, 3, 2, 4, 5 };
+
+            num111.Sort();
+            num222.Sort();
+            bool n1 = num111.SequenceEqual(num222);
+            Console.WriteLine(n1);
+
+            Console.WriteLine("------------------------------------------");
+            List<int> nu = new List<int>() { 1, 2, 3, 4, 5 };
+            List<int> nun = new List<int>() { 1, 3, 2 };
+
+            bool s2= nu.SequenceEqual(nun);
+            Console.WriteLine(s2);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 11.Generation/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11.Generation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // IEnumerable <int> num= new List<int>() {1,2,3,4 };
+            /// IEnumerable<int> num2 = null;
+           //string s= string.Empty; // it give more readiability.
+           IEnumerable<string> bb=new List<string>();
+            IEnumerable<int> number=new List<int>();
+            Console.WriteLine($" count :{number.Count()}");
+
+
+            Console.WriteLine("DefaultIfEmpty : ");
+            IEnumerable<int> number2=Enumerable.DefaultIfEmpty(number); // here it count a default value
+            Console.WriteLine($" count :{number2.Count()}");
+
+            IEnumerable<string> result= Enumerable.DefaultIfEmpty(bb);
+            Console.WriteLine($"count : {bb.Count()}");
+
+            // string value is null so it default value is null so count is also zero.
+
+
+            Console.WriteLine("Empty method");
+
+            IEnumerable<int> result3=Enumerable.Empty<int>();
+            Console.WriteLine($"count : {result3.Count()}");
+            // it give rediability.
+            Console.WriteLine("range method");
+            IEnumerable<int> s = Enumerable.Range(1,345);
+            foreach(int i in s)
+            {
+                Console.Write(i +" ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Repeat");
+            IEnumerable<int> sw = Enumerable.Repeat(20, 12);
+            foreach(int i in sw)
+            {
+                Console.Write(i +" ") ;
+            }
+            Console.WriteLine();
+
+
+            /* IEnumerable<string> result1= Enumerable.DefaultIfEmpty(number);
+            Console.WriteLine($"count : {bb.Count()}");
+*/
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
